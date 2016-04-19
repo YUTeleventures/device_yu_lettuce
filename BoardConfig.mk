4837a8b35c73272f738e9dd63b7923a95fb1f2a1
Abhishek-karmakar <abhishek.karmakar10@gmail.com>
2016-04-19 16:53:11 +0530

    modified readme

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#adding more useless readme. -AK 
+
 -include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/yu/lettuce
